429b9359b09174d20f7dd6e02b0185d20ded3961
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-07-10 08:40:08 +0200

    swapping

--- algo_expert/permutations_second_solution.py
+++ algo_expert/permutations_second_solution.py
@@ -1,28 +1,33 @@
 
 
-def permute(input_arr, perms=None, curr_perm=None, indent=0):
+# TC O(N.N!) SC O(N.N!)
+def permute(input_arr):
 
-    if perms is None:
-        perms = []
+    perms = []   
+    curr_perm = []
+    helper_permute(0, input_arr, perms=perms, indent=0)
+    return perms    
 
-    if curr_perm is None:
-        curr_perm = []
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+
+def helper_permute(idx, input_arr, perms=None, indent=0):
+
     depth = "." * (indent + 1)
     # base case
-    if len(input_arr) == 0 and curr_perm:
-        print(f"{depth} adding the following permutation: {curr_perm}")
-        perms.append(curr_perm)
+    if len(input_arr)-1 == idx:
+        print(f"{depth} adding the following permutation: {input_arr}")
+        perms.append(input_arr.copy())
         
-    
-    # recursive case
-    for i in range(len(input_arr)):
-        print(f"{depth} The input_arr is {input_arr} and the input_arr[i]={input_arr[i]}")
-        # all characters exept the one we remove from the array
-        new_array = input_arr[:i] + input_arr[i+1:]
-        new_perm = curr_perm + [input_arr[i]]
-        permute(new_array, perms, new_perm, indent=indent+1)
+    else:
+        for j in range(idx, len(input_arr)):
+            swap(input_arr, idx, j)
+            helper_permute(idx+1, input_arr, perms, indent=indent+1)
+            swap(input_arr, j, idx)
 
-    return perms    
+    
 
 
         
@@ -37,12 +42,12 @@ def permute(input_arr, perms=None, curr_perm=None, indent=0):
 
 if __name__ == "__main__":
 
-    test = list("ABCD")
+    test = list("ABC")
     res = permute(test)
     print(f"The end result is: \n {[res]}")
     print(f"\n number of permutations is {len(res)}")
 
-    # input_arr = list("AB")
+    # input_arr = list("ABC")
     # print(f"The input array has {input_arr}")
     # # recursive case
     # head = input_arr[0]
