634589be03f1d8bffade03375613e8d3d7d47873
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-07-13 10:25:50 +0200

    depthFirstSearch

---
+++ algo_expert/depth_first_search.js
@@ -0,0 +1,54 @@
+class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    depthFirstSearch(array) {
+      array.push(this.name)
+      if (this.children.length === 0){
+        return;
+      }
+      
+      for (let i=0; i<this.children.length; i++){
+        let child = this.children[i]
+        child.depthFirstSearch(array)
+      }
+      return array;
+    }
+
+    
+  }
+
+//   A
+//   ├─>
+//   B
+//     ├─>
+//     D
+//       └─>
+//         H
+//     └─>
+//     E
+//   └─>
+//   C
+//     ├─>
+//     F
+//     └─>
+//     G
+
+
+  let rootNode = new Node('A');
+rootNode.addChild('B').addChild('C');
+rootNode.children[0].addChild('D').addChild('E');
+rootNode.children[1].addChild('F').addChild('G');
+rootNode.children[0].children[0].addChild('H');
+
+res = rootNode.depthFirstSearch([])
+
+console.log(res)
+  
