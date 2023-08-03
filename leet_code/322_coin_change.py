210e625db8fb3e90d29059c1c27abda3a11268ab
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-08-03 10:16:00 +0200

    recursivve solution

--- leet_code/322_coin_change.py
+++ leet_code/322_coin_change.py
@@ -2,45 +2,72 @@
 from typing import List
 import math
 
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         dp = [math.inf] * (amount + 1)
+#         if amount == 0:
+#             return 0
+#         dp[0] = 0
+#         for coin in coins:
+#             for new_amount in range(1, amount + 1):
+#                 if new_amount >= coin:
+#                     dp[new_amount] = min(dp[new_amount], 1 + dp[new_amount-coin])
+       
+#         return dp[amount] if dp[amount] != math.inf else -1
+
+
+
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:            
+#         def coinChangeInner(rem, cache):
+#             if rem < 0:
+#                 return math.inf
+#             if rem == 0:                    
+#                 return 0       
+#             if rem in cache:
+#                 return cache[rem]
+            
+#             cache[rem] = min(coinChangeInner(rem-x, cache) + 1 for x in coins)                         
+#             return cache[rem]      
+        
+#         ans = coinChangeInner(amount, {})
+#         return -1 if ans == math.inf else ans    
+
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [math.inf] * (amount + 1)
-        if amount == 0:
-            return 0
-        dp[0] = 0
-        for coin in coins:
-            for new_amount in range(1, amount + 1):
-                if new_amount >= coin:
-                    dp[new_amount] = min(dp[new_amount], 1 + dp[new_amount-coin])
-       
-        return dp[amount] if dp[amount] != math.inf else -1
 
-
-
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:            
-        def coinChangeInner(rem, cache):
-            if rem < 0:
-                return math.inf
-            if rem == 0:                    
-                return 0       
-            if rem in cache:
-                return cache[rem]
-            
-            cache[rem] = min(coinChangeInner(rem-x, cache) + 1 for x in coins)                         
-            return cache[rem]      
+        def rec(coins, remain):
+            if remain < 0: return -1
+            if remain == 0: return 0
+            min_count = math.inf
+            for coin in coins:
+                count = rec(coins, remain - coin)
+                if count == -1:
+                    continue
+                min_count = min(min_count, count+1)
+            return min_count if min_count != math.inf else -1
         
-        ans = coinChangeInner(amount, {})
-        return -1 if ans == math.inf else ans    
+        return rec(coins, amount)
+
+
+
+
+
+
+
 
 s = Solution()
 
-# denoms = [1, 5, 10]
-# target = 10
 
-# res = s.coinChange(denoms, target)
-# print(f"denoms {denoms} target={target}")
-# print(f"coinChange {res}")
+
+
+denoms = [1, 5, 10]
+target = 10
+
+res = s.coinChange(denoms, target)
+print(f"denoms {denoms} target={target}")
+print(f"coinChange {res}")
 
 # denoms = [1, 3, 4, 5]
 # target = 7
